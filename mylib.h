c1b54f9f3b3bf7ca627f681df8f215e096ff7b05
DerDieDenDas <126321270+DerDieDenDas@users.noreply.github.com>
2023-02-28 14:56:25 +0200

    Create mylib.h

---
+++ mylib.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <limits>
+#include <random>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::setw;
+using std::setprecision;
+using std::string;
+using std::copy;
+using std::fixed;
+using std::vector;
+using std::swap;
+using std::srand;
